9de198a3c4edd136535887dd16a20aeb77d1ecb8
U-DESKTOP-4G071NR\Simon <simonpavlov2013@gmail.com>
2017-11-12 16:35:01 +0300

    migrate to flask

--- server/__main__.py
+++ server/__main__.py
@@ -1,12 +1,61 @@
-from os import path
+from flask import Flask, jsonify, request
+from flask_jwt import JWT, jwt_required, current_identity
 
-import eve
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+user = User(1, 'user', 'password')
 
 
-def main():
-    app = eve.Eve(settings=path.join(path.dirname(path.realpath(__file__)), 'settings.py'))
-    app.run()
+def authenticate(username, password):
+    if username == user.username and password == user.password:
+        return user
+
+def identity(payload):
+    return user
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
+
+
+# send CORS headers
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+
+
+@app.route('/unprotected')
+def unprotected():
+    return jsonify({
+        'message': 'This is an unprotected resource.'
+    })
+
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return jsonify({
+        'message': 'This is a protected resource.',
+        'current_identity': str(current_identity)
+    })
 
 
 if __name__ == '__main__':
-    main()
+    app.run()
