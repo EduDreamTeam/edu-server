bb8104409c068cc1cbe7962997f3ae6b06a7c371
simon <simonpavlov2013@gmail.com>
2017-12-04 00:00:24 +0300

    updated doc; added db init

--- server/__main__.py
+++ server/__main__.py
@@ -1,15 +1,22 @@
-from flask import Flask, jsonify, request
+import json
+import os
+
+from flask import Flask, request
 from flask_jwt import JWT, jwt_required, current_identity
+from flask_sqlalchemy import SQLAlchemy
 
 
 class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
+    __slots__ = ('id', 'username', 'password')
+
+    def __init__(self, identifier, username, password):
+        self.id = identifier
         self.username = username
         self.password = password
 
     def __str__(self):
-        return "User(id='%s')" % self.id
+        return "User(id='{}')".format(self.id)
+
 
 user = User(1, 'user', 'password')
 
@@ -18,16 +25,21 @@ def authenticate(username, password):
     if username == user.username and password == user.password:
         return user
 
-def identity(payload):
-    return user
 
+def identity(payload):
+    return payload['identity']
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.debug = True
 app.config['SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_MIGRATE_REPO'] = os.path.join(basedir, 'db_repository')
 
 jwt = JWT(app, authenticate, identity)
 
+db = SQLAlchemy(app)
 
 # send CORS headers
 @app.after_request
@@ -41,9 +53,16 @@ def after_request(response):
     return response
 
 
+@app.route('/register', methods=('POST', ))
+def register_new_user():
+    data = json.loads(request.data.decode())
+    print(data)
+    return json.dumps(True)
+
+
 @app.route('/unprotected')
 def unprotected():
-    return jsonify({
+    return json.dumps({
         'message': 'This is an unprotected resource.'
     })
 
@@ -51,7 +70,7 @@ def unprotected():
 @app.route('/protected')
 @jwt_required()
 def protected():
-    return jsonify({
+    return json.dumps({
         'message': 'This is a protected resource.',
         'current_identity': str(current_identity)
     })
