03ff0f13fb429eefbc261317e9464723765d216f
simon <simonpavlov2013@gmail.com>
2017-12-19 01:05:58 +0300

    disabled token expires

--- eduserver/environment.py
+++ eduserver/environment.py
@@ -7,3 +7,5 @@ _package_dir = path.abspath(path.dirname(eduserver.__file__))
 SECRET_KEY = 'super-secret'
 JWT_AUTH_USERNAME_KEY = 'login'
 JWT_AUTH_PASSWORD_KEY = 'password'
+JWT_ACCESS_TOKEN_EXPIRES = False
+JWT_REFRESH_TOKEN_EXPIRES = False
